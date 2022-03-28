bc5fc94735cd4da90c47757fe27ac7084ac0a7f4
DatPhi_Tran <dat-phi.tran@polymtl.ca>
2022-03-27 20:08:48 -0400

    Added fix for tp4

--- TP4_LOG3430/test_crud_madum.py
+++ TP4_LOG3430/test_crud_madum.py
@@ -43,92 +43,119 @@ class TestCrudMadum(unittest.TestCase):
     @patch("crud.CRUD.read_users_file")
     @patch("crud.CRUD.modify_groups_file")
     def test_constructor_crud(self,mock_read_users_file,mock_read_groups_file):
-        mock_read_users_file = self.users_data
-        mock_read_groups_file = self.groups_data
-        crud_obj = CRUD()
-        
+        mock_read_users_file.return_value = self.users_data
+        mock_read_groups_file.return_value = self.groups_data
+        crud_obj = CRUD()        
+        crud_obj.groups_data = self.groups_data
+          
         self.assertEqual(crud_obj.get_groups_data(1, "name"), "friends")
         self.assertEqual(crud_obj.get_groups_data(1, "Trust"), 90)
         self.assertEqual(crud_obj.get_groups_data(1, "List_of_members"), ["alex@gmail.com"])
-       #cas si le groupe default n est pas ajouté
-        self.assertEqual(crud_obj.get_groups_data(2, "name"), "default")
-        self.assertEqual(crud_obj.get_groups_data(2, "Trust"), 50)
-        self.assertEqual(crud_obj.get_groups_data(2, "List_of_members"), [])
+       #cas si le groupe default n'existe pas
+        self.assertEqual(crud_obj.get_groups_data(2, "name"), False)
+        self.assertEqual(crud_obj.get_groups_data(2, "Trust"), False)
+        self.assertEqual(crud_obj.get_groups_data(2, "List_of_members"), False)
+       
     
     # tests des rapporteurs (R) du modele maDUM pour l' attribut groups_data
     @patch("crud.CRUD.read_users_file")
     @patch("crud.CRUD.modify_groups_file")
     def test_get_groups_data(self,mock_read_users_file,mock_read_groups_file):
-        mock_read_users_file = self.users_data
-        mock_read_groups_file = self.groups_data
+        #mock_read_users_file = self.users_data
+        #mock_read_groups_file = self.groups_data
         crud_obj = CRUD()
-        
+        crud_obj.groups_data = self.groups_data
+
         name_group_1 = crud_obj.get_groups_data(1, "name")
         trust_group_1 = crud_obj.get_groups_data(1, "Trust")
         list_members_group_1 = crud_obj.get_groups_data(1, "List_of_members")
-        
-        name_group_2 = crud_obj.get_groups_data(2, "name")
-        trust_group_2 = crud_obj.get_groups_data(2, "Trust")
-        list_members_group_2 = crud_obj.get_groups_data(2, "List_of_members")
         
         self.assertEqual(name_group_1, "friends")
         self.assertEqual(trust_group_1, 90)
         self.assertEqual(list_members_group_1, ["alex@gmail.com"])
         
-        self.assertEqual(name_group_1, "default")
-        self.assertEqual(trust_group_1, 50)
-        self.assertEqual(list_members_group_1, [])
+        
+      # tests des rapporteurs (R) du modele maDUM pour l' attribut groups_data
+    @patch("crud.CRUD.read_users_file")
+    @patch("crud.CRUD.modify_groups_file")
+    def test_get_groups_data(self,mock_read_users_file,mock_read_groups_file):
+        #mock_read_users_file = self.users_data
+        #mock_read_groups_file = self.groups_data
+        crud_obj = CRUD()
+        crud_obj.groups_data = self.groups_data
+
+        name_group_2 = crud_obj.get_groups_data(2, "name")
+        trust_group_2 = crud_obj.get_groups_data(2, "Trust")
+        list_members_group_2 = crud_obj.get_groups_data(2, "List_of_members")
+  
+        
+        self.assertEqual(name_group_2, False)
+        self.assertEqual(trust_group_2, False)
+        self.assertEqual(list_members_group_2, False)
     
     # tests des transformateurs (T) du modele maDUM pour l' attribut groups_data
         #d1
-    @patch("crud.CRUD.read_users_file")
-    @patch("crud.CRUD.modify_groups_file")
-    def test_add_new_group_update_groups_remove_group_remove_group_member(self, mock_read_groups_file, mock_read_users_file):
-        mock_read_users_file.return_value = self.users_data
-        mock_read_groups_file.return_value = self.groups_data
+    def test_add_new_group_update_groups_remove_group_remove_group_member(self):
         crud_obj = CRUD()
-        crud.add_new_group("inf3430", 100, ["yanbra@polymtl.ca"])
-        crud.update_groups(2, "name", "just_inf")
-        crud.remove_group("1")
-        crud.remove_group_member(2, "yanbra@polymtl.ca")
+        #ajout user dans user.json
+        crud_obj.add_new_user("yanbra@polymtl.ca","1998-07-09")
+        crud_obj.add_new_user("datphi@polymtl.ca","1998-07-09")
+        crud_obj.add_new_user("someone@polymtl.ca","1998-07-09")
+      
 
-        self.assertEqual(crud.get_groups_data(1, "name"), False)
-        self.assertEqual(crud.get_groups_data(2, "name"), "inf3430")
-        self.assertEqual(crud.get_groups_data(2, "name"),"just_inf")
-        self.assertEqual(crud.get_groups_data(2, "List_of_members"), [])
-        #d2
-    @patch("crud.CRUD.read_users_file")
-    @patch("crud.CRUD.modify_groups_file")
-    def test_update_groups_add_new_group_remove_group_remove_group_member(self, mock_read_groups_file, mock_read_users_file):
-        mock_read_users_file.return_value = self.users_data
-        mock_read_groups_file.return_value = self.groups_data
-        crud_obj = CRUD()
-        crud.update_groups(1, "Trust", 69)
-        self.assertEqual(crud_objet.get_groups_data(1, "Trust"), 69)
-        crud_obj.add_new_group("inf3430", 99, ["yanbra@polymtl.ca"])
-        crud_obj.remove_group("1")
-        crud_obj.remove_group_member(2, "yanbra@polymtl.ca")
+        crud_obj.add_new_group("Newgroup",50,["datphi@polymtl.ca"])
+        self.assertEqual(crud_obj.get_groups_data(1, "name"), "Newgroup")
 
+        crud_obj.update_groups(1, "name", "just_inf")
+        self.assertEqual(crud_obj.get_groups_data(1, "name"),"just_inf")
+
+        crud_obj.remove_group(1)
         self.assertEqual(crud_obj.get_groups_data(1, "name"), False)
-        self.assertEqual(crud_obj.get_groups_data(2, "name"), "inf3430")
-        self.assertEqual(crud_obj.get_groups_data(2, "Trust"), 99)
-        self.assertEqual(crud_obj.get_groups_data(2, "List_of_members"), [])
-        #d3
-    @patch("crud.CRUD.read_users_file")
-    @patch("crud.CRUD.modify_groups_file")
-    def test_remove_group_add_new_group_update_group_remove_group_member(self, mock_read_groups_file, mock_read_users_file):
-        mock_read_users_file.return_value = self.users_data
-        mock_read_groups_file.return_value = self.groups_data
+
+        crud_obj.remove_group_member(1, "datphi@polymtl.ca")
+        self.assertEqual(crud_obj.get_groups_data(1, "List_of_members"), False)
+    
+        #d2
+    def test_update_groups_add_new_group_remove_group_remove_group_member(self):
         crud_obj = CRUD()
+        #ajout user dans user.json
+        crud_obj.add_new_user("yanbra@polymtl.ca","1998-07-09")
+        crud_obj.add_new_user("datphi@polymtl.ca","1998-07-09")
+        crud_obj.add_new_user("someone@polymtl.ca","1998-07-09")
+      
+
+        crud_obj.update_groups(0, "name", "just_inf")
+        self.assertEqual(crud_obj.get_groups_data(1, "name"),False)
         
-        crud_obj.remove_group("1")
-        self.assertEqual(crud_objet.get_groups_data(1, "Trust"), False)
-        crud_obj.add_new_group("inf3430", 98, ["yanbra@polymtl.ca"])
-        crud.update_groups(1, "Trust", 99)
-        crud_obj.remove_group_member(1, "yanbra@polymtl.ca")
-        self.assertEqual(crud_obj.get_groups_data(1, "name"), "inf3430")
-        self.assertEqual(crud_obj.get_groups_data(1, "Trust"), 99)
+        crud_obj.add_new_group("Newgroup",50,["datphi@polymtl.ca"])
+        self.assertEqual(crud_obj.get_groups_data(1, "name"), "Newgroup")
+
+        crud_obj.remove_group(1)
+        self.assertEqual(crud_obj.get_groups_data(1, "name"), False)
+
+        crud_obj.remove_group_member(1, "datphi@polymtl.ca")
+        self.assertEqual(crud_obj.get_groups_data(1, "List_of_members"), False)
+        #d3
+
+    def test_remove_group_add_new_group_update_group_remove_group_member(self, mock_read_groups_file, mock_read_users_file):
+        crud_obj = CRUD()
+        #ajout user dans user.json
+        crud_obj.add_new_user("yanbra@polymtl.ca","1998-07-09")
+        crud_obj.add_new_user("datphi@polymtl.ca","1998-07-09")
+        crud_obj.add_new_user("someone@polymtl.ca","1998-07-09")
+      
+        crud_obj.remove_group(1)
+        self.assertEqual(crud_obj.get_groups_data(1, "name"), False)
+        
+        crud_obj.add_new_group("Newgroup",50,["datphi@polymtl.ca"])
+        self.assertEqual(crud_obj.get_groups_data(1, "name"), "Newgroup")
+        
+        crud_obj.update_groups(1, "name", "just_inf")
+        self.assertEqual(crud_obj.get_groups_data(1, "name"),"just_inf")
+
+        crud_obj.remove_group_member(1, "datphi@polymtl.ca")
         self.assertEqual(crud_obj.get_groups_data(1, "List_of_members"), [])
+        
      #d4
     @patch("crud.CRUD.read_users_file")
     @patch("crud.CRUD.modify_groups_file")
@@ -140,8 +167,8 @@ class TestCrudMadum(unittest.TestCase):
         crud_obj.add_new_group("inf3430", 98, ["yanbra@polymtl.ca"])
         
         crud_obj.remove_group("1")
-        self.assertEqual(crud_objet.get_groups_data(1, "Trust"), False)
-        crud.update_groups(2, "Trust", 99)
+        self.assertEqual(crud_obj.get_groups_data(1, "Trust"), False)
+        crud_obj.update_groups(2, "Trust", 99)
         crud_obj.remove_group_member(1, "yanbra@polymtl.ca")
         self.assertEqual(crud_obj.get_groups_data(2, "name"), "inf3430")
         self.assertEqual(crud_obj.get_groups_data(2, "Trust"), 99)
